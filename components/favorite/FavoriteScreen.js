379d4e8318219701f60d5a703a96933a16ba1ff6
Bùi Trí Thức <buitrithuc1008@gmail.com>
2023-06-22 00:08:03 +0700

    update favorite screen

--- components/favorite/FavoriteScreen.js
+++ components/favorite/FavoriteScreen.js
@@ -6,9 +6,11 @@ import {
   StyleSheet,
   ImageBackground,
   TouchableOpacity,
+  Button,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useIsFocused } from "@react-navigation/native";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function Favourite({ navigation }) {
   const [listFavourite, setListFavourite] = useState([]);
@@ -32,69 +34,98 @@ export default function Favourite({ navigation }) {
     }
   };
 
+  const removeItem = async (itemId) => {
+    try {
+      const updatedFavorites = listFavourite.filter(
+        (item) => item.id !== itemId
+      );
+      await AsyncStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+      setListFavourite(updatedFavorites);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const removeAllItems = async () => {
+    try {
+      await AsyncStorage.removeItem("favorites");
+      setListFavourite([]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     getFavouriteList();
   }, [isFocused]);
 
   if (listFavourite.length === 0) {
     return (
-      <View>
+      <View style={styles.screenempty}>
         <Text style={styles.textNot}>Your favorites list is empty</Text>
       </View>
     );
   }
 
   return (
-    <FlatList
-      data={listFavourite}
-      renderItem={({ item }) => (
-        <TouchableOpacity onPress={() => handlePress(item)}>
-          <View style={styles.listItem}>
-            <ImageBackground
-              source={{ uri: item.image.uri }}
-              style={styles.imageBackground}
-            ></ImageBackground>
-            <View style={styles.namedesc}>
-              <Text style={styles.name}>{item.name}</Text>
-              <Text style={styles.desc}>{item.description}</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={listFavourite}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => handlePress(item)}>
+            <View style={styles.listItem}>
+              <ImageBackground
+                source={{ uri: item.image.uri }}
+                style={styles.imageBackground}
+              ></ImageBackground>
+              <View style={styles.namedesc}>
+                <Text style={styles.name}>{item.name}</Text>
+                <Text style={styles.desc}>{item.description}</Text>
+              </View>
+              <TouchableOpacity onPress={() => removeItem(item.id)}>
+                <FontAwesome name="trash" size={20} color="red" />
+              </TouchableOpacity>
             </View>
-          </View>
-        </TouchableOpacity>
-      )}
-      keyExtractor={(item) => item.id}
-    />
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      />
+      <Button title="Remove All" onPress={removeAllItems} />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  listItem: {
+    flexDirection: "row",
+    marginVertical: 8,
+    backgroundColor: "#FCF8EE",
+    borderWidth: 0.3,
+    alignItems: "center",
+    padding: 8,
+  },
+  namedesc: {
+    flex: 1,
+    marginLeft: 12,
+  },
+  desc: {
+    textAlign: "justify",
+  },
+  screenempty: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
-  listItem: {
-    display: "flex",
-    flexDirection: "row",
-    margin: 8,
-    backgroundColor: "#FCF8EE",
-    borderWidth: 0.3,
-  },
-  namedesc: {
-    marginTop: -5,
-  },
-  desc: {
-    marginRight: 106,
-    textAlign: "justify",
-    marginLeft: 12,
-  },
   textNot: {
-    fontSize: 30,
+    fontSize: 20,
+    textAlign: "center",
   },
   name: {
-    marginTop: 3,
     fontWeight: "bold",
     fontSize: 20,
-    marginLeft: 12,
   },
   imageBackground: {
     width: 100,
